a97ce515e398fe844e93bb6c1eaee9bc0907445e
mohdehiya <mohamedehiyakamal12216@gmail.com>
2025-08-28 17:02:26 +0530

    Update listinteger.java

--- listinteger.java
+++ listinteger.java
@@ -8,20 +8,15 @@ public class listinteger {
    public static void main(String[] args) {
             
 	   List<List<Integer>> listOfLists = new ArrayList();
-
-       // First inner list
        List<Integer> list1 = Arrays.asList(1, 2, 3);
-       // Second inner list
+       
        List<Integer> list2 = Arrays.asList(4, 5);
-       // Third inner list
+    
        List<Integer> list3 = Arrays.asList(6, 7, 8, 9);
 
-       // Add them to the big list
        listOfLists.add(list1);
        listOfLists.add(list2);
        listOfLists.add(list3);
-
-       // Print
        System.out.println(listOfLists);
    }
      }
